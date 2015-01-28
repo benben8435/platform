28c8f52e21602e527d122c912497aa04f8849e59
Allen <benben8435@gmail.com>
2015-01-27 17:41:54 -0800

    Fine.

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,14 @@
 Platform::Application.routes.draw do
-  devise_for :users
+  get "welcome/index"
   resources :rents
 
+  devise_for :users, controllers: { sessions: "users/sessions" }
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
-
+  root 'welcome#index'
+  get 'rents' => 'rents#index', as: :user_root
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
